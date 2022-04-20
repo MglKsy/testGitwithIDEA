7dc8f924a38061fce042f4a71c50b1bfecc9f173
mgl <2529947582@qq.com>
2022-04-20 11:05:21 +0800

    hot_first_test

--- src/main/java/MyTest.java
+++ src/main/java/MyTest.java
@@ -4,5 +4,6 @@ public class MyTest {
         System.out.println("the_second");
         System.out.println("the_third");
         System.out.println("hot_fix_the_first");
+        System.out.println("hot_fix_test");
     }
 }
