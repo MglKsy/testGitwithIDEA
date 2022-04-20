a1d35f3c5c84cb2c2cc836cb18c6d771266d001c
mgl <2529947582@qq.com>
2022-04-20 10:06:30 +0800

    third

--- src/main/java/MyTest.java
+++ src/main/java/MyTest.java
@@ -2,5 +2,6 @@ public class MyTest {
     public static void main(String[] args) {
         System.out.println("the_first");
         System.out.println("the_second");
+        System.out.println("the_third");
     }
 }
